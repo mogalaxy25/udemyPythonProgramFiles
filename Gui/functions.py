81b2ee037c522bc8af268b1a1be64251da833b4e
mogalaxy25 <44922047+mogalaxy25@users.noreply.github.com>
2024-10-15 18:52:04 -0400

    Implemented the Add button

--- Gui/functions.py
+++ Gui/functions.py
@@ -1,10 +1,12 @@
-def get_todos(filepath='todos.txt'):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """Read a text file and return the list of to-do items"""
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
-def write_todos(todos_arg, filepath='todos.txt'):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ Write the to-do items list in the file"""
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
